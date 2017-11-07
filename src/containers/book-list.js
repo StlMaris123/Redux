f4681a9922fb4e42f8af9b495468b6ed935da9bc
StlMaris123 <stellamarisnjage@gmail.com>
2017-11-07 17:37:58 +0300

    form connection between redux and the container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component{
+class BookList extends Component{
     renderList(){
         return this.props.books.map((book) => {
             return(
@@ -18,5 +19,11 @@ export default class BookList extends Component{
             {this.renderList()}
          <ul>
         )
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        books: state.books
     }
 }
